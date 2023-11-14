442e151e3eefbbd3276fe78bf3a4106468dc5d22
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-14 17:37:39 +0200

    feat: add vite-plugin-svg-sprite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import createSvgSpritePlugin from "vite-plugin-svg-sprite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createSvgSpritePlugin({
+      symbolId: "icon-[name]-[hash]",
+    }),
+  ],
+});
