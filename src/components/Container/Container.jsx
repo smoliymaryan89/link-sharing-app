2b0e932a15f3cc3a14f23fc88d10827a56772826
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-14 17:42:42 +0200

    feat: add prop types for container component

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,9 +1,15 @@
+import PropTypes from "prop-types";
+
 const Container = ({ children }) => {
   return (
     <div className="max-w-[375px] px-4 mx-auto md:max-w-[768px] md:px-6 lg:max-w-[1440px]">
       {children}
     </div>
   );
+};
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default Container;
