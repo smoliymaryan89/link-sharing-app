cc96cc45f8027264699d59f83c1d2f6055af1a8a
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-14 12:01:50 +0200

    chore: add basic configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-grey": "#FAFAFA",
+        "dark-grey": "#333333",
+        grey: "#737373",
+        blue: "#633CFF",
+        "light-purple": "#EFEBFF",
+        active: "#BEADFF",
+        disabled: "rgba(99, 60, 255, 0.25)",
+        red: "#FF3939",
+      },
+      fontFamily: {
+        sans: ["Instrument Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
