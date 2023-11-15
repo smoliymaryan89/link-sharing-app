9cbfa269f229db988d075692aa623f7484712255
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-15 14:05:57 +0200

    feat: add login validation

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -9,3 +9,10 @@ export const registerValidationSchema = Yup.object({
     .oneOf([Yup.ref("password"), null], "Passwords must match.")
     .required("Can't be empty"),
 });
+
+export const loginValidationSchema = Yup.object({
+  email: Yup.string().email("Invalid email address").required("Can't be empty"),
+  password: Yup.string()
+    .min(8, "At least 8 characters")
+    .required("Can't be empty"),
+});
