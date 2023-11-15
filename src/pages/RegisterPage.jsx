f4eafe4e0f6f8c9ea54a7484b65b3f67fbb2651b
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-15 13:27:08 +0200

    feat: create registration page

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,31 @@
+import sprite from "../assets/icons/sprite.svg";
+
+import Container from "../components/Container/Container";
+import InfoPanel from "../components/InfoPanel/InfoPanel";
+import RegisterForm from "../components/RegisterForm/RegisterForm";
+
+const RegisterPage = () => {
+  return (
+    <Container className={"py-[32px]  max-w-[343px] "}>
+      <div className="flex items-center md:justify-center gap-[10px] mb-[64px] md:mb-[98px]">
+        <svg className="block" width={40} height={40}>
+          <use href={`#${sprite}_logo`}></use>
+        </svg>
+        <p className="text-[32px] text-dark-grey font-bold placeholder:opacity-[0.5]">
+          devlinks
+        </p>
+      </div>
+
+      <div className="md:max-w-[476px] py-[40px] px-[40px] bg-white rounded-[10px] mx-auto">
+        <InfoPanel
+          title={"Create account"}
+          text={"Let's get you started sharing your links!"}
+        />
+
+        <RegisterForm />
+      </div>
+    </Container>
+  );
+};
+
+export default RegisterPage;
