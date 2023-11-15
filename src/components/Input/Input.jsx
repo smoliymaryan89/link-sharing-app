df167ff9994e4becb0d234e4b2613969458063e2
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-15 13:23:53 +0200

    feat: create input component

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,46 @@
+import PropTypes from "prop-types";
+
+import sprite from "../../assets/icons/sprite.svg";
+
+const Input = ({
+  id,
+  name,
+  type,
+  onChange,
+  value,
+  className,
+  placeholder,
+  icon,
+  iconStyle,
+}) => {
+  return (
+    <div className={`relative  flex items-center`}>
+      <svg className={`absolute top-[16px] left-[16px] ${iconStyle}`}>
+        <use href={`#${sprite}_${icon}`}></use>
+      </svg>
+      <input
+        id={id}
+        name={name}
+        type={type}
+        onChange={onChange}
+        value={value}
+        placeholder={placeholder}
+        className={`rounded-[8px] w-full border-input border-[1px] outline-none text-[16px] leading-[1.5] text-dark-grey py-[12px] pl-[44px] pr-[44px] ${className}`}
+      />
+    </div>
+  );
+};
+
+Input.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  className: PropTypes.string,
+  placeholder: PropTypes.string,
+  icon: PropTypes.string,
+  iconStyle: PropTypes.string,
+};
+
+export default Input;
