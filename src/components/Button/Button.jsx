cf226ad5aaf66c4e09ab02fbee91536ca7e2179f
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-15 21:33:13 +0200

    feat(Button): add props

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,22 @@
 import PropTypes from "prop-types";
 
-const Button = ({ className, type, title }) => {
+const Button = ({ className, type, title, disabled }) => {
   return (
     <button
-      className={` w-full rounded-[8px] py-[11px] px-[80px] bg-blue text-white font-semibold ${className}`}
+      className={` w-full rounded-[8px] py-[11px] px-[80px] bg-blue  font-semibold ${className}`}
       type={type}
+      disabled={disabled}
     >
       {title}
     </button>
   );
 };
 
-Button.propTypes = {};
+Button.propTypes = {
+  className: PropTypes.string,
+  type: PropTypes.oneOf(["button", "submit", "reset"]).isRequired,
+  title: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+};
 
 export default Button;
