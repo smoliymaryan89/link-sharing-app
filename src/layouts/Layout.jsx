271ca622f1d6f2851c4ef0e7fbbcb0882d0eea48
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-17 17:32:38 +0200

    chore: update layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,21 +1,20 @@
 import { Outlet } from "react-router-dom";
+
 import Header from "../components/Header/Header";
-import { Suspense } from "react";
-import useWindowSize from "../hooks/useWindowSize";
 import PhonePreview from "../components/PhonePreview/PhonePreview";
+
+import useWindowSize from "../hooks/useWindowSize";
 
 const Layout = () => {
   const { width } = useWindowSize();
 
   return (
     <>
-      <Header></Header>
+      <Header />
 
-      <main className="flex ">
+      <main className="flex">
         {width >= 1200 && <PhonePreview />}
-        <Suspense>
-          <Outlet />
-        </Suspense>
+        <Outlet />
       </main>
     </>
   );
