5a5431f8be9a4d39b42e4989041e4cb53843bdb4
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-27 00:32:13 +0200

    feat: create user selectors

--- src/redux/user/userSelectors.js
+++ src/redux/user/userSelectors.js
@@ -1,3 +1,7 @@
 export const selectIsLoading = (state) => state.user.isLoading;
 
 export const selectUser = (state) => state.user.user;
+
+export const selectError = (state) => state.user.error;
+
+export const selectUserAvatar = (state) => state.user.user.image;
