882b300b80083fa7c989495e3e070a8581fcc934
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-26 03:39:33 +0200

    update: update link list

--- src/components/LinkList/LinkList.jsx
+++ src/components/LinkList/LinkList.jsx
@@ -3,15 +3,13 @@ import { selectLinks } from "../../redux/link/linkSelectors";
 
 import LinkItem from "./LinkItem/LinkItem";
 
-const LinkList = ({ handleDelete }) => {
+const LinkList = () => {
   const links = useSelector(selectLinks);
-
-  console.log("links", links);
 
   return (
     <ul>
       {links.map((item) => (
-        <LinkItem key={item.id} item={item} handleDelete={handleDelete} />
+        <LinkItem key={item.id} item={item} />
       ))}
     </ul>
   );
