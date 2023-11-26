1faf2d7d2eea1f97577c45a7aeb6e6f05a2810f1
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-26 03:19:44 +0200

    feat: create select data

--- src/utils/selectData.js
+++ src/utils/selectData.js
@@ -1,22 +1,79 @@
 import sprite from "../assets/icons/sprite.svg";
 
 const links = [
-  { value: "github", label: "GitHub", icon: `#${sprite}_gihub` },
-  { value: "twitter", label: "Twitter", icon: `#${sprite}_twitter` },
-  { value: "linkedin", label: "LinkedIn", icon: `#${sprite}_linkedin` },
-  { value: "youtube", label: "YouTube", icon: `#${sprite}_youtube` },
-  { value: "facebook", label: "Facebook", icon: `#${sprite}_facebook` },
-  { value: "twitch", label: "Twitch", icon: `#${sprite}_twitch` },
-  { value: "codewars", label: "Codewars", icon: `#${sprite}_codewars` },
-  { value: "codepen", label: "Codepen", icon: `#${sprite}_codepan` },
+  {
+    value: "github",
+    label: "GitHub",
+    icon: `#${sprite}_gihub`,
+    color: "bg-[#1A1A1A]",
+  },
+  {
+    value: "twitter",
+    label: "Twitter",
+    icon: `#${sprite}_twitter`,
+    color: "bg-[#43B7E9]",
+  },
+  {
+    value: "linkedin",
+    label: "LinkedIn",
+    icon: `#${sprite}_linkedin`,
+    color: "bg-[#2D68FF]",
+  },
+  {
+    value: "youtube",
+    label: "YouTube",
+    icon: `#${sprite}_youtube`,
+    color: "bg-[#EE3939]",
+  },
+  {
+    value: "facebook",
+    label: "Facebook",
+    icon: `#${sprite}_facebook`,
+    color: "bg-[#2442AC]",
+  },
+  {
+    value: "twitch",
+    label: "Twitch",
+    icon: `#${sprite}_twitch`,
+    color: "bg-[#A970FF]",
+  },
+  {
+    value: "codewars",
+    label: "Codewars",
+    icon: `#${sprite}_codewars`,
+    color: "bg-[#8A1A50]",
+  },
+  // TODO change color
+  {
+    value: "codepen",
+    label: "Codepen",
+    icon: `#${sprite}_codepan`,
+    color: "bg-[#EE3FC8]",
+  },
   {
     value: "freecodecamp",
     label: "freeCodeCamp",
     icon: `#${sprite}_freecodecamp`,
+    color: "bg-[#302267]",
   },
-  { value: "gitlab", label: "GitLab", icon: `#${sprite}_gitlab` },
-  { value: "hashnode", label: "Hashnode", icon: `#${sprite}_hashnode` },
-  { value: "stackoverflow", label: "Stack Overflow", icon: `#${sprite}_stack` },
+  {
+    value: "gitlab",
+    label: "GitLab",
+    icon: `#${sprite}_gitlab`,
+    color: "bg-[#EB4925]",
+  },
+  {
+    value: "hashnode",
+    label: "Hashnode",
+    icon: `#${sprite}_hashnode`,
+    color: "bg-[#0330D1]",
+  },
+  {
+    value: "stackoverflow",
+    label: "Stack Overflow",
+    icon: `#${sprite}_stack`,
+    color: "bg-[#EC7100]",
+  },
 ];
 
 export default links;
