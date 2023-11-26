50565022b9425da834e696fd863c53e6075cd58c
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-27 00:39:55 +0200

    feat: create PreviewContent component

---
+++ src/components/PreviewContent/PreviewContent.jsx
@@ -0,0 +1,41 @@
+import { useSelector } from "react-redux";
+import { selectLinks } from "../../redux/link/linkSelectors";
+import { selectUser } from "../../redux/user/userSelectors";
+import PreviewItem from "../LinkList/PreviewItem/PreviewItem";
+
+const PreviewContent = () => {
+  const links = useSelector(selectLinks);
+  const { image, lastName, firstName, emailPreview } = useSelector(selectUser);
+
+  return (
+    <div className=" max-w-[350px] mx-auto md:py-[48px] md:px-[56px] md:rounded-[24px] md:bg-white md:shadow-card-shadow ">
+      <div className="mb-[56px]">
+        {image ? (
+          <img
+            src={image}
+            className="rounded-full w-[104px] h-[104px] object-cover border-[4px] border-blue mb-[25px] mx-auto"
+          />
+        ) : (
+          <div className="rounded-full w-[104px] h-[104px] bg-dark-white mb-[25px] mx-auto"></div>
+        )}
+
+        <ul>
+          <li className="text-[32px] font-bold text-dark-grey text-center mb-[8px]">
+            {firstName && firstName} {lastName && lastName}
+          </li>
+          <li className="text-center">{emailPreview && emailPreview}</li>
+        </ul>
+      </div>
+
+      {links.length > 0 && (
+        <ul className="flex flex-col items-center">
+          {links.map(({ id, platform, url }) => (
+            <PreviewItem key={id} platform={platform} url={url} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default PreviewContent;
