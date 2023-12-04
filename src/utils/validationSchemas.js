1170f55820328d84fa36867c96b7634aea2c2987
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-12-04 16:40:08 +0200

    feat: add validation for profile form

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,7 +1,11 @@
 import * as Yup from "Yup";
 
+const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+
 export const registerValidationSchema = Yup.object({
-  email: Yup.string().email("Invalid email address").required("Can't be empty"),
+  email: Yup.string()
+    .matches(emailRegex, "Invalid email address")
+    .required("Can't be empty"),
   password: Yup.string()
     .min(8, "At least 8 characters")
     .required("Can't be empty"),
@@ -11,8 +15,23 @@ export const registerValidationSchema = Yup.object({
 });
 
 export const loginValidationSchema = Yup.object({
-  email: Yup.string().email("Invalid email address").required("Can't be empty"),
+  email: Yup.string()
+    .matches(emailRegex, "Invalid email address")
+    .required("Can't be empty"),
   password: Yup.string()
     .min(8, "At least 8 characters")
     .required("Can't be empty"),
+});
+
+export const profileValidationSchema = Yup.object().shape({
+  firstName: Yup.string().matches(
+    /^[A-Za-z]+$/,
+    "First name should only contain letters"
+  ),
+
+  lastName: Yup.string().matches(
+    /^[A-Za-z]+$/,
+    "Last name should only contain letters"
+  ),
+  emailPreview: Yup.string().matches(emailRegex, "Invalid email address"),
 });
