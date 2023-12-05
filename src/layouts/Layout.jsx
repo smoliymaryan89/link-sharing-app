40ece6dfb2d2e44881f273e78c9bf33a2d5d2263
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-12-05 02:37:47 +0200

    refactor: remove useWindowSize hook

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,17 +3,13 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import PhonePreview from "../components/PhonePreview/PhonePreview";
 
-import useWindowSize from "../hooks/useWindowSize";
-
 const Layout = () => {
-  const { width } = useWindowSize();
-
   return (
     <>
       <Header />
 
       <main className="flex">
-        {width >= 1200 && <PhonePreview />}
+        <PhonePreview />
         <Outlet />
       </main>
     </>
