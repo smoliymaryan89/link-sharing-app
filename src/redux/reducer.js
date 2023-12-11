27bd7c8da8a8e8dbeb2d0842f379352932bcb783
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-12-11 14:46:35 +0200

    feat: add shared reducer to store

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,9 +1,11 @@
 import { authReducer } from "./auth/authSlice";
 import { linkReducer } from "./link/linkSlice";
+import { sharedReducer } from "./shared/sharedSlice";
 import { userReducer } from "./user/userSlice";
 
 export const reducer = {
   auth: authReducer,
   user: userReducer,
   link: linkReducer,
+  shared: sharedReducer,
 };
