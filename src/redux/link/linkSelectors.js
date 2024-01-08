c648d600ca426bb4ac140c495a323ba843fea736
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-01-08 19:24:53 +0200

    feat: add select for reordered links

--- src/redux/link/linkSelectors.js
+++ src/redux/link/linkSelectors.js
@@ -2,4 +2,6 @@ export const selectLinks = (state) => state.link.links;
 
 export const selectPreviewLinks = (state) => state.link.previewLinks;
 
+export const selectReorderedLinks = (state) => state.link.reorderedLinks;
+
 export const selectIsLoading = (state) => state.link.isLoading;
